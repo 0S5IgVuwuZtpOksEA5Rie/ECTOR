786a0f305efc5e006597f54a20b9a5c37512b253
0S5IgVuwuZtpOksEA5Rie <3QZWZtfVCyVX6v@gmail.com>
2023-01-06 23:29:42 +1000

    completed function

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
-use itertools;
 use rand::{self, random};
 use std::{self, io::stdin};
 
 fn break_settings_string_to_array(settings_string: &str) -> (bool, u128, i128, i128, i128) {
-    let settings_string_split: Vec<&str> = settings_string.split(";").collect::<Vec<&str>>();
+    let settings_string_split: Vec<&str> = settings_string.split(";").collect();
 
-    return;
+    if settings_string_split.len() != 4 {
+        return (false, 0, 0, 0, 0);
+    }
+
+    for i in 0..settings_string_split.len() {
+        if settings_string_split[i].parse::<u128>().is_err()
+            || settings_string_split[i].parse::<i128>().is_err()
+        {
+            return (false, 0, 0, 0, 0);
+        }
+    }
+
+    return (
+        true,
+        settings_string_split[0].parse::<u128>().unwrap(),
+        settings_string_split[1].parse::<i128>().unwrap(),
+        settings_string_split[2].parse::<i128>().unwrap(),
+        settings_string_split[3].parse::<i128>().unwrap(),
+    );
 }
 
 fn generate_random_setting() -> (u128, i128, i128, i128) {
