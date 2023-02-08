79af0d4b7af7a03a4672a387057c3746070ac724
Symbol <3qzwztfvcyvx6v@gmail.com>
2023-02-08 22:14:40 +1000

    more work

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,24 @@
-use rand;
+use rand::random;
 use std::io::stdin;
 
-macro_rules! input {
-    ($prompt: block) => {
-        println!($prompt);
+fn input(prompt: &str) -> String {
+    println!("{}", prompt);
 
-        let mut string_input: String = String::new();
+    let mut string_input: String = String::new();
 
-        stdin()
-            .read_line(&mut string_input)
-            .expect("input read error");
+    stdin()
+        .read_line(&mut string_input)
+        .expect("input read error");
 
-        string_input = String::from(string_input.trim());
+    string_input = String::from(string_input.trim());
 
-        return string_input;
-    };
+    return string_input;
 }
 
+// True:  encrypt,  False:  decrypt
 fn get_direction() -> bool {
     loop {
-        let string_input: String = input!("encrypt or decrypt?  (e/d)");
+        let string_input: String = input("encrypt or decrypt?  (e/d)");
 
         if string_input == String::from("e") {
             return true;
@@ -33,15 +32,7 @@ fn get_direction() -> bool {
 
 fn get_rerun_or_close() -> bool {
     loop {
-        println!("close?  (y/n)");
-
-        let mut string_input: String = String::new();
-
-        stdin()
-            .read_line(&mut string_input)
-            .expect("input read error");
-
-        string_input = String::from(string_input.trim());
+        let string_input: String = input("close?  (y/n)");
 
         if string_input == String::from("y") {
             return false;
@@ -53,9 +44,32 @@ fn get_rerun_or_close() -> bool {
     }
 }
 
-fn read_user_setting() {}
+fn interpret_user_setting() {}
 
-fn generate_random_setting() {}
+fn read_user_setting(direction: bool) -> i128 {
+    loop {
+        if direction == true {
+            print!("type \"g\" for generated settings");
+        }
+        let input: String = input("insert settings:");
+
+        if direction == true {
+            if input == String::from("r") {
+                return generate_random_setting();
+            } else {
+                return interpret_user_setting();
+            }
+        } else {
+            return interpret_user_setting();
+        }
+
+        println!("invalid input");
+    }
+}
+
+fn generate_random_setting() -> i128 {
+    return random::<i128>();
+}
 
 fn main() {
     println!("ECTOR EnCrypTOR");
@@ -65,7 +79,9 @@ fn main() {
         let encrypt: bool = get_direction();
 
         if encrypt == true {
+            let key: i128 = read_user_setting(true);
         } else {
+            let key: i128 = read_user_setting(false);
         }
 
         run = get_rerun_or_close();
